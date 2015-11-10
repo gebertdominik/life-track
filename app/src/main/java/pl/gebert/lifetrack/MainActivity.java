e94dafa479e80a8fd61a851422e0f14d64d230fd
Dominik Gebert <dgebert91@gmail.com>
2015-11-11 00:02:37 +0100

    Add initial project files

---
+++ app/src/main/java/pl/gebert/lifetrack/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.gebert.lifetrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
